8c0821a473076bf5e57147895be849d2b03366f8
ugurgurel <ugurgurel@gmail.com>
2023-04-02 08:07:00 +0300

    development

--- app/index.js
+++ app/index.js
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images } from "../constants";
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>HOme dasdsadas </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Text>fdasdfsda</Text>
+      <Stack.Screen />
+    </SafeAreaView>
   );
 };
 
